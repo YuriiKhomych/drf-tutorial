6683073a24138d89383f299f2b764ed1d1697f95
yuriikhomych <yuriykhomich@gmail.com>
2018-12-06 10:05:55 +0200

    Change mixin to generic CBV

--- snippets/views.py
+++ snippets/views.py
@@ -1,34 +1,14 @@
-from rest_framework.mixins import (ListModelMixin,
-                                   CreateModelMixin,
-                                   RetrieveModelMixin,
-                                   UpdateModelMixin,
-                                   DestroyModelMixin)
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(ListModelMixin, CreateModelMixin, GenericAPIView):
+class SnippetList(ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class SnippetDetail(RetrieveModelMixin, UpdateModelMixin,
-                    DestroyModelMixin, GenericAPIView):
+class SnippetDetail(RetrieveDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
